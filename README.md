cfb7eb52c4f28c1d0673a7645f1540d42d82e0e3
Muhammad Ashir <mashir670@gmail.com>
2024-06-04 20:27:50 +0500

    async await demo

---
+++ README.md
@@ -0,0 +1,14 @@
+# Async Await JS Demo
+
+This is a short async await demo in Javascript that I've prepared for FixelCould.
+
+
+### Tech used:
+ - Vanilla JS
+ - node.js
+ - node fetch API
+
+
+### Note:
+
+If you have any questions or if this demonstration strays away from the required practice, do reach out!
---
+++ main.js
@@ -0,0 +1,50 @@
+
+
+// async function getEmployees() {
+//   for (let i = 0; i < 10; i++) {
+//     try {
+//     console.log('Fetching employees', i, 'times');
+//     const employees = await fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json());
+//     employees.forEach(employee => {
+//       console.log(employee.name);
+//     });
+//     setTimeout(() => {
+//       console.log('Waiting for 3 seconds');
+//     }, 3000);
+//     }
+//     catch (error) {
+//       console.log('Error:', error);
+//     }
+//   }
+// }
+
+
+function timeout(ms) {
+  console.log('Waiting for', ms, 'milliseconds \n');
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function getEmployees() {
+  for (let i = 0; i < 10; i++) {
+    try {
+      console.log('Fetching employees', i+1 , 'times');
+      const employees = await fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json());
+      employees.forEach(employee => {
+        console.log(employee.name);
+      });
+      await timeout(2000);
+    }
+    catch (error) {
+      console.log('Error:', error);
+    }
+  }
+}
+
+
+function main() {
+  getEmployees();
+  
+}
+
+
+main();
